766ed6cbbbc4eca4655255d051d90d862a8a4939
paskuale <pellicanipasquale@gmail.com>
2018-11-26 09:05:51 +0100

    TCPDF main file added

---
+++ TCPDF.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @copyright Copyright &copy;2018 Pasquale Pellicani
+ * @company Iris Soluzioni Srl
+ * @website http://www.irissoluzioni.it
+ * @package yii2-tcpdf
+ * @github https://github.com/paskuale75/yii2-tcpdf
+ * @license GNU GENERAL PUBLIC LICENSE VERSION 3
+ * @tcpdf library 6.0.075
+ * @tcpdf documentation http://www.tcpdf.org/docs.php
+ * @tcpdf examples http://www.tcpdf.org/examples.php
+ */
+
+namespace hbalkhi\tcpdf;
+
+use Yii;
+use yii\base\Component;
+
+/*                                     *
+ *  Component to load TCPDF Libraries  *
+ *                                     */
+
+class TCPDF extends Component
+{
+
+    public function __construct()
+    {
+        // Include the main TCPDF library
+        require_once(dirname(__FILE__).'/tcpdf/tcpdf.php');
+    }
+
+}
